c68d67235ffe5fc646c4b20d719cb565e1d8864a
l-fakih <139354341+l-fakih@users.noreply.github.com>
2025-05-10 00:01:33 +0300

    use reduce object method to help in finding the oldest + add another function generate total years lived

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+    const totalYears = function(birth, death){
+        if (!death){
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
+    
+    const findTheOldest = function(people) {
+        return people.reduce((oldPerson, currentPerson) => {
+            const currentAge = totalYears(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+            const oldAge = totalYears(oldPerson.yearOfBirth, oldPerson.yearOfDeath);
+            if (oldAge > currentAge){
+                return oldPerson;
+            }
+            else{
+                return currentPerson;
+            }
+    
+        });
+    };
 
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+    // Do not edit below this line
+    module.exports = findTheOldest;
